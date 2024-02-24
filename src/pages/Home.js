1c3569a062ce7721882f41158f9106e29827950e
20020450 <tranhainam1410@gmail.com>
2024-02-25 00:34:45 +0700

    test

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,7 @@
-import React from "react";
-import { signOut } from "firebase/auth";
-import { auth } from "../firebase";
-import { useNavigate } from "react-router-dom";
 import a from "assets/img/a.jpg";
+import React from "react";
 
 const Home = () => {
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    signOut(auth)
-      .then(() => {
-        // Sign-out successful.
-        navigate("/login");
-        console.log("Signed out successfully");
-      })
-      .catch((error) => {
-        // An error happened.
-      });
-  };
-
   return (
     <div>
       <img src={a} alt="a" style={{ objectFit: "cover", width: "100%" }}></img>

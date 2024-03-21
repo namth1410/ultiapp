68611b27e67c05889b0863f4160fd2cba335d958
namth1410 <tranhainam1410@gmail.com>
2024-03-21 09:43:40 +0700

    feat: UI newfeed empty

--- src/pages/Class/NewsFeed/NewsFeed.js
+++ src/pages/Class/NewsFeed/NewsFeed.js
@@ -11,6 +11,7 @@ import {
 import { useEffect, useState } from "react";
 import { auth, firestore } from "../../../firebase";
 import styles from "./NewsFeed.module.css";
+import { ReactComponent as NewsFeedSvg } from "assets/img/newsfeed.svg";
 
 function NewsFeed() {
   const classId = window.location.pathname.split("/")[2];
@@ -58,6 +59,32 @@ function NewsFeed() {
       {canPostNews && (
         <div className={styles.wrapper_form_create}>
           <FormCreateNews></FormCreateNews>
+        </div>
+      )}
+
+      {!newsfeed && (
+        <div className={styles.intro}>
+          <div className={styles.a1}>
+            <NewsFeedSvg />
+          </div>
+
+          <p
+            style={{
+              textAlign: "center",
+            }}
+          >
+            Bảng tin
+          </p>
+          <p
+            style={{
+              fontSize: "14px",
+              fontWeight: "400",
+              textAlign: "center",
+              marginTop: "5px"
+            }}
+          >
+            Nơi trao đổi các vấn đề trong lớp học dành cho giáo viên học sinh
+          </p>
         </div>
       )}
 
